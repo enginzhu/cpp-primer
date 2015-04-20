ffefef829e57ff7f245127c264ab7a86a654d738
enginzhu <enginzhu@gmail.com>
2015-04-20 23:54:57 +0800

    exercise for using stream iterator with algorithm

---
+++ chapter10/10.30.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::sort;
+using std::copy;
+using std::istream_iterator;
+using std::ostream_iterator;
+using std::back_inserter;
+
+
+int main(int argc, char* argv[])
+{
+	istream_iterator<int> i_iter(cin), eof;
+	vector<int> ivec;
+	copy(i_iter, eof, back_inserter(ivec));
+	sort(ivec.begin(), ivec.end());
+	ostream_iterator<int> out(cout, " ");
+	for (auto &i : ivec)
+	{
+		*out++ = i;
+	}
+	cout << endl;
+}
